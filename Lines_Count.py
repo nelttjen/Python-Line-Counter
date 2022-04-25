ed2d4922e7d7b4d81199bcec87efbf0932c66bf8
NelttjeN <tatyapa0880@yandex.ru>
2022-04-25 12:15:31 +0500

    v2

--- Lines_Count.py
+++ Lines_Count.py
@@ -1,21 +1,21 @@
 import os
 
 
-def count_lines_on_project():
+def count_lines_on_project(extension='.py'):
     walker = os.walk('./')
-    all_files = [[i[0], i[2]] for i in walker if not i[0].startswith('./.git') and not i[0].startswith('./build')
-                 and not i[0].startswith('./dist') and not i[0].startswith('./venv') and i[2]]
+    banned = ['./.git', './build', './migrate', './dist', './venv']
+    all_files = [[i[0], i[2]] for i in walker if not any([i[0].startswith(j) for j in banned]) and i[2]]
     count = 0
-    py_files = []
+    files = []
     for i, cell in enumerate(all_files):
         temp = []
         for j in cell[1]:
-            if j[-3:] == '.py':
+            if j.endswith(extension):
                 temp.append(j)
         if temp:
             path = all_files[i][0].replace('\\', '/')
-            py_files.append([path, temp])
-    for i in py_files[1:]:
+            files.append([path, temp])
+    for i in files[1:]:
         path = i[0]
         for file in i[1]:
             if file != 'Lines_Count.py':
@@ -23,4 +23,8 @@ def count_lines_on_project():
                     lines = [i for i in f.readlines() if i and i != '\n']
                     lines = [i for i in lines if not i.lstrip().startswith('#')]
                     count += len(lines)
-    print(f'Counted lines of code (.py only, without comments): {count}')
+    print(f'Counted lines of code (without python comments): {count}')
+
+
+if __name__ == '__main__':
+    count_lines_on_project()
