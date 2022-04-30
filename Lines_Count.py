f9c3617e748160419c11e598e8d7e6075b57b8e3
Artemy <tatyapa0880@yandex.ru>
2022-04-30 14:15:19 +0500

    v2.1

--- Lines_Count.py
+++ Lines_Count.py
@@ -1,9 +1,9 @@
 import os
 
 
-def count_lines_on_project(extension='.py'):
+def count_lines_on_project(extension='.py', ret_val='str'):
     walker = os.walk('./')
-    banned = ['./.git', './build', './migrate', './dist', './venv']
+    banned = ['./.git', './build', './migrations', './dist', './venv']
     all_files = [[i[0], i[2]] for i in walker if not any([i[0].startswith(j) for j in banned]) and i[2]]
     count = 0
     files = []
@@ -15,16 +15,24 @@ def count_lines_on_project(extension='.py'):
         if temp:
             path = all_files[i][0].replace('\\', '/')
             files.append([path, temp])
-    for i in files[1:]:
+    # print(files)
+    for i in files:
         path = i[0]
         for file in i[1]:
-            if file != 'Lines_Count.py':
+            if file != 'line_counter.py':
                 with open(f'{path}/{file}', 'r', encoding='utf-8') as f:
-                    lines = [i for i in f.readlines() if i and i != '\n']
+                    lines = [i for i in f.read().split('\n') if i and i != '\n']
                     lines = [i for i in lines if not i.lstrip().startswith('#')]
                     count += len(lines)
-    print(f'Counted lines of code (without python comments): {count}')
+    if ret_val == "str":
+        return f'Counted lines of code (without python comments): {count}'
+    elif ret_val == 'int':
+        return count
+    return count
 
 
 if __name__ == '__main__':
-    count_lines_on_project()
+    print('total (html, css, js, py):', count_lines_on_project('.py', ret_val='int') +
+          count_lines_on_project('.js', ret_val='int') +
+          count_lines_on_project('.css', ret_val='int') +
+          count_lines_on_project('.html', ret_val='int'), 'lines')
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 <h3>To get this thing work:</h3><br>
 Put in on the top level of your folders (main project folder) and import to your code<br>
-Call the method and count of lines will be in <em>stdout</em>
+Call the method and count of lines will returned as <em>string</em>
+You can also change return method to <em>int</em>
